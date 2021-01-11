1e899b67902d6f2809460f7c24cb23f2fd07d692
coloradoe <rxdelper@gmail.com>
2021-01-11 13:02:07 -0600

    Fix variables file for s3, remove terraworkspace from this repo

--- s3/variables.tf
+++ s3/variables.tf
@@ -1,12 +1,7 @@
 variable "s3" {
   type = object ({
-    bucket_name = string
-    environment_tag = string
+    bucket_name = string,
+    environment_tag = string,
     workspace = string
-})
-default = ({
-    bucket_name = "bucket-rdp-488-demodemodemo7328"
-    environment_tag = "Test" 
-    workspace = "us-east-2"
   })
 }
